9c9b83d425b7e2dcd079192d6741bff5e8d67bd0
atiravseth <atiravsethwork@gmail.com>
2026-01-22 03:12:47 +0530

    v1_lab1

---
+++ Lab1/stretch_api.py
@@ -0,0 +1,55 @@
+import stretch_body.robot
+import numpy as np
+
+robot = stretch_body.robot.Robot()
+robot.startup()
+
+#stow the robot
+robot.stow()
+robot.push_command()
+
+#move the arm and lift to max positions
+robot.arm.move_to(0.55)
+robot.lift.move_to(0.11)
+robot.push_command()
+robot.arm.wait_until_at_setpoint()
+
+#From docs:
+"""The gripper uses the same move_to(name, pos) API, 
+and accepts values between -100 to 100, where the gripper 
+is fully closed at -100 and fully open at 100."""
+#move the wrist to different positions, one joint at a time
+
+robot.end_of_arm.move_to('wrist_yaw', 50)
+robot.push_command()
+robot.end_of_arm.move_to('wrist_pitch', -50)
+robot.push_command()
+robot.end_of_arm.move_to('wrist_roll', 50)
+robot.push_command()
+robot.end_of_arm.move_to('stretch_gripper',50)
+robot.push_command()
+robot.end_of_arm.move_to('stretch_gripper',-50)
+robot.push_command()
+
+#move the head
+robot.head.move_by('head_pan', np.radians(40)) 
+robot.push_command()
+robot.head.move_by('head_tilt', np.radians(30)) 
+robot.push_command()
+robot.head.wait_until_at_setpoint()
+
+#stow the robot again
+robot.stow()
+robot.push_command()
+robot.wait_until_at_setpoint()
+
+#move the robot base
+robot.base.translate_by(0.5)
+robot.push_command()
+robot.base.wait_until_at_setpoint()
+robot.base.rotate_by(np.radians(30))
+robot.push_command()
+robot.base.wait_until_at_setpoint()
+robot.base.translate_by(-0.5)
+robot.push_command()
+robot.base.wait_until_at_setpoint()
