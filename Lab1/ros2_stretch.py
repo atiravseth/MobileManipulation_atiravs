126a0115f5ec1aad63851c470037aff54d330ffb
atiravseth <atiravsethwork@gmail.com>
2026-01-29 03:07:09 +0530

    end of Buffer class Lab-1

--- Lab1/ros2_stretch.py
+++ Lab1/ros2_stretch.py
@@ -1,4 +1,5 @@
 import hello_helpers.hello_misc as hm
+import numpy as np
 
 class MyNode(hm.HelloNode):
     def __init__(self):
@@ -7,14 +8,32 @@ class MyNode(hm.HelloNode):
     def main(self):
         hm.HelloNode.main(self, 'my_node', 'my_node', wait_for_first_pointcloud=False)
         # my_node's main logic goes here
-        hm.move_to_pose({'joint_arm': 0.7}, blocking=True)
+
+        #stow the robot
+        hm.stow_the_robot() 
+
+        #move the arm and lift to maximum positions
+        hm.move_to_pose({'joint_arm': 0.55, 'joint_lift': 1.1}, blocking=True)
+
+        #move the wrist to different positions, one joint at a time
+        hm.move_to_pose({'joint_wrist_yaw': np.radians(50)}, blocking=True)
+        hm.move_to_pose({'joint_wrist_pitch': np.radians(-50)}, blocking=True)
+        hm.move_to_pose({'joint_wrist_roll': np.radians(50)}, blocking=True)
+        hm.move_to_pose({'joint_gripper_finger_left': 50}, blocking=True)
+        hm.move_to_pose({'joint_gripper_finger_right': -50}, blocking=True)
+
+        #move the head
+        hm.move_to_pose({'joint_head_pan': hm.get_joint_position('joint_head_pan') + np.radians(40)}, blocking=True)
+        hm.move_to_pose({'joint_head_tilt': hm.get_joint_position('joint_head_tilt') + np.radians(30)}, blocking=True)  
+
+        #stow the robot again
+        hm.stow_the_robot()
+
+        #move the robot base
+        hm.move_base_by(0.5, 0.0, blocking=True)
+        hm.rotate_base_by(np.radians(180), blocking=True)
+        hm.move_base_by(0.5, 0.0, blocking=True)
 
 node = MyNode()
 node.main()
 
-####
-node.stow_the_robot() 
-node.move_to_pose({'joint_arm': 0.7}, blocking=True) 
-node.move_to_pose({‘joint_wrist_yaw': 
-           node.joint_state.position[node.joint_state.name.index(‘joint_wrist_yaw')] +  
-           np.radians(45)}, blocking=True)
