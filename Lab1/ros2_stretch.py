d910ad7cdeb07115fab54b0be7b4da5e18736ffe
atiravseth <atiravsethwork@gmail.com>
2026-01-30 00:55:41 +0530

    lab visit -4, ros file corrected

--- Lab1/ros2_stretch.py
+++ Lab1/ros2_stretch.py
@@ -1,39 +1,35 @@
 import hello_helpers.hello_misc as hm
 import numpy as np
 
-class MyNode(hm.HelloNode):
-    def __init__(self):
-        hm.HelloNode.__init__(self)
+node = hm.HelloNode.quick_create('node')
 
-    def main(self):
-        hm.HelloNode.main(self, 'my_node', 'my_node', wait_for_first_pointcloud=False)
-        # my_node's main logic goes here
+try:
+    #stow
+    node.stow_the_robot()
+    
+    #move the arm and lift to maximum positions
+    node.move_to_pose({'joint_arm': 0.55, 'joint_lift': 1.1}, blocking=True)
 
-        #stow the robot
-        hm.stow_the_robot() 
+    #move the wrist to different positions, one joint at a time
+    node.move_to_pose({'joint_wrist_yaw': np.radians(50)}, blocking=True)
+    node.move_to_pose({'joint_wrist_pitch': np.radians(-50)}, blocking=True)
+    node.move_to_pose({'joint_wrist_roll': np.radians(50)}, blocking=True)
+    node.move_to_pose({'joint_gripper_finger_left': 50}, blocking=True)
+    node.move_to_pose({'joint_gripper_finger_right': -50}, blocking=True)   
 
-        #move the arm and lift to maximum positions
-        hm.move_to_pose({'joint_arm': 0.55, 'joint_lift': 1.1}, blocking=True)
+    #move the head
+    node.move_to_pose({'joint_head_pan': node.get_joint_position('joint_head_pan') + np.radians(40)}, blocking=True)
+    node.move_to_pose({'joint_head_tilt': node.get_joint_position('joint_head_tilt') + np.radians(30)}, blocking=True)
 
-        #move the wrist to different positions, one joint at a time
-        hm.move_to_pose({'joint_wrist_yaw': np.radians(50)}, blocking=True)
-        hm.move_to_pose({'joint_wrist_pitch': np.radians(-50)}, blocking=True)
-        hm.move_to_pose({'joint_wrist_roll': np.radians(50)}, blocking=True)
-        hm.move_to_pose({'joint_gripper_finger_left': 50}, blocking=True)
-        hm.move_to_pose({'joint_gripper_finger_right': -50}, blocking=True)
+    #stow again
+    node.stow_the_robot()
 
-        #move the head
-        hm.move_to_pose({'joint_head_pan': hm.get_joint_position('joint_head_pan') + np.radians(40)}, blocking=True)
-        hm.move_to_pose({'joint_head_tilt': hm.get_joint_position('joint_head_tilt') + np.radians(30)}, blocking=True)  
+    #move the robot base
+    node.move_base_by(0.5, 0.0, blocking=True)
+    node.rotate_base_by(np.radians(180), blocking=True)
+    node.move_base_by(0.5, 0.0, blocking=True)  
 
-        #stow the robot again
-        hm.stow_the_robot()
-
-        #move the robot base
-        hm.move_base_by(0.5, 0.0, blocking=True)
-        hm.rotate_base_by(np.radians(180), blocking=True)
-        hm.move_base_by(0.5, 0.0, blocking=True)
-
-node = MyNode()
-node.main()
-
+finally:
+    node.stop_the_robot()
+    
+    # Your main logic here
