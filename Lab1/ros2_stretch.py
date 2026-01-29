e591ccb2a838bbae6a1c21e4b81b60bef06ec8ef
atiravseth <atiravsethwork@gmail.com>
2026-01-30 01:21:39 +0530

    lab visit -4, ros file corrected 3

--- Lab1/ros2_stretch.py
+++ Lab1/ros2_stretch.py
@@ -14,14 +14,14 @@ try:
     node.move_to_pose({'joint_wrist_yaw': np.radians(50)}, blocking=True)
     node.move_to_pose({'joint_wrist_pitch': np.radians(-50)}, blocking=True)
     node.move_to_pose({'joint_wrist_roll': np.radians(50)}, blocking=True)
-    node.move_to_pose({'joint_gripper_finger_left': 50}, blocking=True)
-    node.move_to_pose({'joint_gripper_finger_right': -50}, blocking=True)   
+    node.move_to_pose({'joint_gripper_finger_left': 0.165}, blocking=True)
+    node.move_to_pose({'joint_gripper_finger_right': -0.35}, blocking=True)   
 
     #move the head
     node.move_to_pose({'joint_head_pan': np.radians(40)}, blocking=True)
     # node.move_to_pose({'joint_head_pan': node.get_joint_position('joint_head_pan') + np.radians(40)}, blocking=True)
 
-    node.move_to_pose({'joint_head_tilt': np.radians(30)}, blocking=True)
+    node.move_to_pose({'joint_head_tilt': np.radians(20)}, blocking=True)
     # node.move_to_pose({'joint_head_tilt': node.get_joint_position('joint_head_tilt') + np.radians(30)}, blocking=True)
 
     #stow again
