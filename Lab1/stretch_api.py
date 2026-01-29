09035509746ec636824cf0167d838fcf2f7326f0
atiravseth <atiravsethwork@gmail.com>
2026-01-30 00:09:01 +0530

    lab visit -2

--- Lab1/stretch_api.py
+++ Lab1/stretch_api.py
@@ -12,7 +12,7 @@ robot.stow()
 robot.arm.move_to(0.55)
 robot.lift.move_to(1.1)
 robot.push_command()
-robot.arm.wait_until_at_setpoint() #try it may not work
+robot.wait_command() #try it may not work
 
 #From docs:
 """The gripper uses the same move_to(name, pos) API, 
@@ -22,32 +22,32 @@ is fully closed at -100 and fully open at 100."""
 
 robot.end_of_arm.move_to('wrist_yaw', 50)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 robot.end_of_arm.move_to('wrist_pitch', -50)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 robot.end_of_arm.move_to('wrist_roll', 50)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 robot.end_of_arm.move_to('stretch_gripper',50)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 robot.end_of_arm.move_to('stretch_gripper',-50)
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 
 #move the head
 robot.head.move_by('head_pan', np.radians(40)) 
 robot.push_command()
-robot.arm.wait_until_at_setpoint()
+robot.wait_command()
 robot.head.move_by('head_tilt', np.radians(30)) 
 robot.push_command()
-robot.head.wait_until_at_setpoint()
+robot.wait_command()
 
 #stow the robot again
 robot.stow()
 robot.push_command()
-robot.wait_until_at_setpoint()
+robot.wait_command()
 
 #move the robot base
 robot.base.translate_by(0.5)
